b079bc16ed97c9d6be82d6a4a50e023c336677f4
Arthri <41360489+Arthri@users.noreply.github.com>
2021-08-01 14:54:26 +0800

    feat: Add description

--- src/ListPluginsPlugin/Properties/AssemblyInfo.cs
+++ src/ListPluginsPlugin/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ListPluginsPlugin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Provides commands for listing the currently loaded plugins")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Arthri")]
 [assembly: AssemblyProduct("ListPluginsPlugin")]
