530a2c5e2304f77577b740ca0592fa90e78290a8
Arthri <41360489+Arthri@users.noreply.github.com>
2021-08-01 15:58:45 +0800

    fix: Purple sucks

--- src/ListPluginsPlugin/Plugin.cs
+++ src/ListPluginsPlugin/Plugin.cs
@@ -55,7 +55,7 @@ namespace ListPluginsPlugin
         private void ListPlugins(CommandArgs args)
         {
             // It's in ABGR format
-            var color = Color.Purple.packedValue;
+            var color = Color.BlueViolet.packedValue;
 
             // Flip color, because Terraria uses RGB
             color = ((color & 0x000000FF) << 16) | (color & 0x0000FF00) | ((color & 0x00FF0000) >> 16);
